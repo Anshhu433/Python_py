ffe9315519130f2334b4cc59de91680f74c0bf11
Anshhu433 <86539686+Anshhu433@users.noreply.github.com>
2024-09-24 15:14:17 +0000

    chai.py

---
+++ 01_Basics/chai.py
@@ -0,0 +1,9 @@
+# Importing method(greet) from another file(Hello_greet)
+
+from Hello_chai import greet
+
+greet("Ansh");
+
+chai_one = 'Cutting';
+chai_two = 'Lemon Tea';
+chai_three = 'Ginger Tea';
