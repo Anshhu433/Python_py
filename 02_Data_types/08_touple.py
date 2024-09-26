a8c4bc7c115de9302ea0078a5879a6f7aaf38159
Anshhu433 <86539686+Anshhu433@users.noreply.github.com>
2024-09-26 17:04:48 +0000

    08_tuple.py

---
+++ 02_Data_types/08_touple.py
@@ -0,0 +1,18 @@
+# Touple
+# Tuples are used to store multiple items in a single variable.
+# The major difference between 'Touple' and 'List' is Touple are immutable and  'List' are mutable
+
+#Touple can contain different data type 
+
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+print(tuple1 , tuple2 , tuple3);
+
+
+# Lets chk the datatype of the tuple
+
+mytuple = ("apple", "banana", "cherry")
+print(type(mytuple))
+
+print(len(tuple1));     #Way to check the length of the touple.
