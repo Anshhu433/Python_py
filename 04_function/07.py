248de34ac5c337c75a93e1720453fd60a3559138
Anshhu433 <86539686+Anshhu433@users.noreply.github.com>
2024-09-29 16:40:58 +0000

    07.py

--- 04_function/07.py
+++ 04_function/07.py
@@ -3,8 +3,24 @@
 
 #write a generator function that yeilds the even numbers upto a specified limit 
 
+# def even(limit):
+#     for i in range(2 , limit+1 , 2):
+#         return i
+
+# for num in even(10):
+#     print(num);           #Here '2' will be printed always. because Once return is used, the function's execution is over, 
+                          #and it cannot resume from where it left off.
+
+
+
+# yeild :  yield is used in a function to return a value and pause the function's execution, allowing it to resume later 
+# from where it left off.
+
+# Now we will create the function using yeild
+
 def even(limit):
     for i in range(2 , limit+1 , 2):
-        print(i)
+        yield i
 
-even(10);
+for num in even(10):
+   print(num);
